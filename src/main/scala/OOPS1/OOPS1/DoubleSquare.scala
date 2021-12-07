9e4594016be66d04d486e88b068d641c51dba02c
panditshubham489 <Shubham.s@knoldus.com>
2021-12-07 18:36:50 +0530

    Assignment

---
+++ src/main/scala/OOPS1/OOPS1/DoubleSquare.scala
@@ -0,0 +1,25 @@
+package OOPS1
+import scala.collection.mutable.ListBuffer
+
+
+//To insert double of an element
+class DoubleQueue(queue : ListBuffer[Int]) extends Queue {
+
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * 2)
+  }
+
+}
+
+//To insert Square of an element
+class SquareQueue(queue: ListBuffer[Int]) extends Queue {
+
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * elementToBePushed)
+  }
+
+
+
+}
